30fdf2984ad4055d237997b8388fd4a98b8ce7dd
mr433 <max.rodewald@gmail.com>
2017-03-22 10:12:33 -0600

    converting to es6/cleaning up code

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var express = require('express')
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const express = require('express')
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(express.static('src'));
 
@@ -12,13 +11,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    var room;
+io.on('connection', socket => {
+    let room;
 
-    socket.on('room creation', (roomName) => {
+    socket.on('room creation', roomName => {
         socket.join(roomName);
         room = roomName;
-        var clients = io.sockets.adapter.rooms[roomName].sockets;
         io.to(room).emit('user join', io.sockets.adapter.rooms[room] ? io.sockets.adapter.rooms[room].length : 0);
     });
 
@@ -26,16 +24,14 @@ io.on('connection', (socket) => {
         io.to(room).emit('user disconnect', io.sockets.adapter.rooms[room] ? io.sockets.adapter.rooms[room].length : 0);
     });
 
-    socket.on('chat message', (msg) => {
+    socket.on('chat message', msg => {
         io.to(room).emit('chat message', msg);
     });
 
-    socket.on('user typing', (user) => {
+    socket.on('user typing', user => {
         io.to(room).emit('user typing', user);
     });
 
 });
 
-http.listen(3000, () => {
-    console.log('listening on 3000');
-})
+http.listen(3000, () => console.log('listening on 3000'));
